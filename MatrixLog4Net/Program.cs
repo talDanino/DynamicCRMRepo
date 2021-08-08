d85f654adc74f1b084e92919f1f802b3ccfa7e56
talDanino <taldanino1196@gmail.com>
2021-08-09 00:34:29 +0300

    First commit

---
+++ MatrixLog4Net/Program.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+namespace MatrixLog4Net
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+        static void Main(string[] args)
+        {
+            log4netFunction();
+            Console.ReadLine();
+        }
+
+        public static void log4netFunction()
+        {
+            int input;
+            var watch = new System.Diagnostics.Stopwatch();
+
+            Console.WriteLine("Please enter a number: ");
+            input = int.Parse(Console.ReadLine());
+
+            GlobalContext.Properties["INPUT"] = input;
+
+            watch.Start();
+
+            for (int i = 0; i < input; i++)
+            {
+                continue;
+            }
+
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+            log.Info("");
+        }
+    }
+}
